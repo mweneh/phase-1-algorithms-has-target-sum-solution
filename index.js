37491afcb704f94c47bd070d4cc51f316000d267
David Ongaro <davidongaroh@gmil.com>
2022-06-16 20:16:23 +0300

    Done

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers= {}
+  for (const num of array){
+    //console.log(seenNumbers)
+    const complement = target- num
+
+    if (complement in seenNumbers) return true
+    seenNumbers[num] = true
+
+  }
+  return false 
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+  create an object to track seen numbers
+  iterate through each number
+  identify complement for current number adding to target 
+  check if our complement is a key in our seenNumbers
+  if so return true else add it to the object 
+
 */
 
 /*
